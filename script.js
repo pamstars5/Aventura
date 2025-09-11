d58ad6c1d3489988665fb0e0630e9e2ff396bceb
pamstars5 <pamela.lauterio.oliveira@escola.pr.gov.br>
2025-09-11 08:45:21 -0300

    ok

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ avanca.forEach(button => {
 button.addEventListener('click',function(){
     const atual = document.querySelector('.ativo');
     const proximoPasso= 'passo-'+this.getAttribute('data-proximo');
-})
+    atual.classList.remove('ativo');
+    const proximoElemento= document.getElementById(proximoPasso);
 
-
+    if (proximoElemento){
+    proximoElemento.classList.add('ativo')
+} else {
+    console.error(`Elemento com ID "${proximoPasso}" não encontrado.`)
 }
 
 
-)
+
+
+
+})
+});
